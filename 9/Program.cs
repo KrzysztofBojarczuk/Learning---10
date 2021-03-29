0e78ac891ed3981f416e4531c4ebccaf38b228af
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-03-29 13:38:14 +0200

    Add project files.

---
+++ 9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double hits;
+                double atBat;
+                double battingAverage;
+
+                Console.WriteLine("Wprowadź liczbę udanych odbić zawodnika.");
+                hits = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Wprowadź liczbę wszystkich prób odbicia piłki przez zawodnika.");
+                atBat = int.Parse(Console.ReadLine());
+
+                battingAverage = hits / atBat;
+
+
+                Console.WriteLine($"Skutecznośc odbijania piłki przez zawodnika wynosi {battingAverage}.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wprowadź poprawne dane.");
+            }
+
+        }
+    }
+}
